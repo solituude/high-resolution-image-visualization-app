f1b49bc2c0c060e8667ca875737156d9e553a952
solituude <dbelousova70@gmail.com>
2023-10-26 18:43:26 +0400

    Добавлена лупа на базе билинейной субпиксельной интерполяции

--- src/Canvas/Canvas.jsx
+++ src/Canvas/Canvas.jsx
@@ -1,16 +1,107 @@
 import React, {useState, useEffect} from 'react';
 import '../App.css';
-import {getConvertedBrightnessArray, getInitialBrightnessArray} from "../functions/functionaityCore";
+import {
+    findMinMaxValue,
+    getConvertedBrightnessArray,
+    getInitialBrightnessArray, getInterpolateArray, getNearestNeighbor, getNormalizeColorArray,
+    getReducedInitBrightness
+} from "../functions/functionaityCore";
 
 
 const Canvas = ({file, shift}) => {
     const [image, setImage] = useState(null); // Создаем состояние для хранения изображения
+    const [overviewImage, setOverviewImage] = useState(null);
+
+    const [zoom, setZoom] = useState(2);
     const [brightnessMatrix, setBrightnessMatrix] = useState([]); // матрица яркости для дальнейшнего определения яркости пикселя по координате
 
-    const [imageSize, setImageSize] = useState({height: 0, width: 0}); // размер изображения
 
-    // Функция для создания изображения из матрицы яркости
-    const createImageFromMatrix = () => {
+    // const [convertedBrightnessMatrix, setConvertedBrightnessMatrix] = useState([]);
+    const [imageSize, setImageSize] = useState({height: 0, width: 0}); // размер изображения
+    const canvas = document.createElement('canvas'); // создание элемента холста в приложении
+    const overviewCanvas = document.createElement('canvas'); // создание холста для обзорного изображения
+    const magnifierCanvas = document.createElement('canvas');
+    magnifierCanvas.height = 300;
+    magnifierCanvas.width = 300;
+    const magnifierContext = magnifierCanvas.getContext('2d');
+    let magnifierImageData = magnifierContext.createImageData(magnifierCanvas.width, magnifierCanvas.width);
+    const containerMagnifier = document.getElementById('magnifier-container');
+
+    // координата наведенного пикселя изображения
+    const [hoveredPixelCoordinatesPicture, setHoveredPixelCoordinatesPicture] = useState({x: 0, y: 0});
+    // координата наведенного пикселя контейнера, в котором находится изображение
+    const [hoveredPixelCoordinatesContainer, setHoveredPixelCoordinatesContainer] = useState({x: 0, y: 0});
+    // яркость наведенного пикселя
+    const [hoveredPixelBrightness, setHoveredPixelBrightness] = useState(0);
+
+    const [isNormalize, setIsNormalize] = useState(false);
+    const [isInterpolate, setIsInterpolate] = useState(false);
+
+    // создание обзорного изображения
+    const createOverviewImage = (initialBrightnessArray, shift) => {
+        const reducedBrightnessArray = getReducedInitBrightness(initialBrightnessArray);
+        const convertedColor = getConvertedBrightnessArray(reducedBrightnessArray, shift);
+        overviewCanvas.height = convertedColor.length;
+        overviewCanvas.width = convertedColor[0].length;
+
+        const overviewContext = overviewCanvas.getContext('2d'); // контекст созданного холста для дальнейшего взаимодествия с ним
+        const overviewImageData = overviewContext.createImageData(convertedColor[0].length, convertedColor.length); // создание объекта ImageData с указанымми размерами (все пиксели черные прозрачные при инициализации)
+
+        for (let y = 0; y < convertedColor.length; y++) { // цикл по высоте изображения
+            for (let x = 0; x < convertedColor[0].length; x++) { // уикл по ширине изображения
+                const brightness = convertedColor[y][x]; // яркость пикселя
+                const index = (y * convertedColor[0].length + x) * 4; // вычисление индекса для заполнения объекта ImageData
+
+                overviewImageData.data[index] = brightness;
+                overviewImageData.data[index + 1] = brightness;
+                overviewImageData.data[index + 2] = brightness;
+                overviewImageData.data[index + 3] = 255;
+            }
+        }
+        overviewContext.putImageData(overviewImageData, 0, 0); // отрисовка данных на холсте начиная с координат (0,0)
+        const dataURL = overviewCanvas.toDataURL();
+        const overviewImg = new Image(); // создание элемента изображения
+        overviewImg.src = dataURL;
+
+        setOverviewImage(overviewImg);
+    }
+
+    // создание изображения
+    const createImage = (initialColor, selectedOption) => {
+        let convertedColor = getConvertedBrightnessArray(initialColor, selectedOption); // получение матрицы яркости пикселей изображения для дальнейшего построения изображения в RGB
+        const imageHeight = convertedColor.length; // высота изображения
+        const imageWidth = convertedColor[0].length; // ширина изображения
+
+        setImageSize({height: imageHeight, width: imageWidth}); // назначение размеров изображения в состояние, для отображения в инетрфейсе
+
+        canvas.width = imageWidth; // назначение ширины изображения
+        canvas.height = imageHeight; // назначение высоты изображения
+
+        const ctx = canvas.getContext('2d'); // контекст созданного холста для дальнейшего взаимодествия с ним
+
+        const imageData = ctx.createImageData(imageWidth, imageHeight); // создание объекта ImageData с указанымми размерами (все пиксели черные прозрачные при инициализации)
+
+        for (let y = 0; y < imageHeight; y++) { // цикл по высоте изображения
+            for (let x = 0; x < imageWidth; x++) { // уикл по ширине изображения
+                const brightness = convertedColor[y][x]; // яркость пикселя
+                const index = (y * imageWidth + x) * 4; // вычисление индекса для заполнения объекта ImageData
+
+                imageData.data[index] = brightness; // присваивание красного спектра
+                imageData.data[index + 1] = brightness; // присваивание зеленого спектра
+                imageData.data[index + 2] = brightness; // присваивание синего спектра
+                imageData.data[index + 3] = 255; // присваивание прозрачности пикселя
+            }
+        }
+        ctx.putImageData(imageData, 0, 0); // отрисовка данных на холсте начиная с координат (0,0)
+        const dataURL = canvas.toDataURL();
+        const img = new Image(); // создание элемента изображения
+        img.src = dataURL;
+
+        setImage(img);
+    }
+
+    // Функция обработки события загрузки файла
+    const handleUploadFile = () => {
         if (file) { // проверка на загрузку файла
             const reader = new FileReader(); // объект, обладающий возможностями взаимодейтвия с файлами
             reader.onload = function (e) { // обработчик события чтения файла
@@ -18,52 +109,13 @@ const Canvas = ({file, shift}) => {
                 setBrightnessMatrix(initialColor); // назначение матрицы яркости массива созданной выше, для удобвства в дальнейшем использовании
 
                 const selectedOption = parseInt(shift); // выбранный сдвиг
-                let convertedColor = getConvertedBrightnessArray(initialColor, selectedOption); // получение матрицы яркости пикселей изображения для дальнейшего построения изображения в RGB
 
-                const imageHeight = convertedColor.length; // высота изображения
-                const imageWidth = convertedColor[0].length; // ширина изображения
-
-                setImageSize({height: imageHeight, width: imageWidth}); // назначение размеров изображения в состояние, для отображения в инетрфейсе
-
-                const canvas = document.createElement('canvas'); // создание элемента холста в приложении
-                canvas.width = imageWidth; // назначение ширины изображения
-                canvas.height = imageHeight; // назначение высоты изображения
-
-                const ctx = canvas.getContext('2d'); // контекст созданного холста для дальнейшего взаимодествия с ним
-
-                const imageData = ctx.createImageData(imageWidth, imageHeight); // создание объекта ImageData с указанымми размерами (все пиксели черные прозрачные при инициализации)
-
-                for (let y = 0; y < imageHeight; y++) { // цикл по высоте изображения
-                    for (let x = 0; x < imageWidth; x++) { // уикл по ширине изображения
-                        const brightness = convertedColor[y][x]; // яркость пикселя
-                        const index = (y * imageWidth + x) * 4; // вычисление индекса для заполнения объекта ImageData
-
-                        imageData.data[index] = brightness.Red; // присваивание красного спектра
-                        imageData.data[index + 1] = brightness.Green; // присваивание зеленого спектра
-                        imageData.data[index + 2] = brightness.Blue; // присваивание синего спектра
-                        imageData.data[index + 3] = 255; // присваивание прозрачности пикселя
-                    }
-                }
-                ctx.putImageData(imageData, 0, 0); // отрисовка данных на холсте начиная с координат (0,0)
-                const dataURL = canvas.toDataURL();
-                const img = new Image(); // создание элемента изображения
-                img.src = dataURL;
-
-                setImage(img);
+                createImage(initialColor, selectedOption)
+                createOverviewImage(initialColor, selectedOption);
             };
             reader.readAsArrayBuffer(file);
         }
-
     }
-
-    // координата наведенного пикселя изображения
-    const [hoveredPixelCoordinatesPicture, setHoveredPixelCoordinatesPicture] = useState({x: 0, y: 0});
-
-    // координата наведенного пикселя контейнера, в котором находится изображение
-    const [hoveredPixelCoordinatesContainer, setHoveredPixelCoordinatesContainer] = useState({x: 0, y: 0});
-
-    // яркость наведенного пикселя
-    const [hoveredPixelBrightness, setHoveredPixelBrightness] = useState(0);
 
 
     // назначение координаты картинки
@@ -73,7 +125,6 @@ const Canvas = ({file, shift}) => {
         } catch (e) {
             console.log(e.message);
         }
-
     }
 
     // назначение яркости при наведениии на картинку
@@ -83,6 +134,52 @@ const Canvas = ({file, shift}) => {
         } catch (e) {
             console.log(e.message);
         }
+    }
+
+    const setMagnifierPicture = (currX, currY) => {
+        const delta = magnifierCanvas.height % zoom === 0 ? Math.floor((magnifierCanvas.height) / (2 * zoom)) :
+            Math.ceil((magnifierCanvas.height) / (2 * zoom));
+
+        const context = magnifierCanvas.getContext('2d');
+        const reducedArray = brightnessMatrix.slice(currY - delta, currY + delta + 1).map(row =>
+            row.slice(currX - delta, currX + delta + 1));
+        const minMaxBrightness = findMinMaxValue(reducedArray);
+
+        const existingMagnifierCanvas = containerMagnifier.querySelector('canvas');
+        if (existingMagnifierCanvas) {
+            containerMagnifier.removeChild(existingMagnifierCanvas);
+        }
+        let convertedReducedArray;
+
+        if (isNormalize) {
+            convertedReducedArray = getNormalizeColorArray(reducedArray, minMaxBrightness.minValue, minMaxBrightness.maxValue);
+            convertedReducedArray = getConvertedBrightnessArray(convertedReducedArray, 0);
+        } else{
+            convertedReducedArray = getConvertedBrightnessArray(reducedArray, parseInt(shift));
+        }
+
+        convertedReducedArray = getNearestNeighbor(convertedReducedArray, zoom,
+            (convertedReducedArray.length * zoom - magnifierCanvas.height) / 2);
+
+        if (isInterpolate) {
+            convertedReducedArray = getInterpolateArray(zoom, convertedReducedArray, );
+        }
+
+
+
+        for (let y = 0; y < convertedReducedArray.length; y++) {
+            for (let x = 0; x < convertedReducedArray[0].length; x++) {
+                const brightness = convertedReducedArray[y][x]; // яркость пикселя
+                const index = (y * convertedReducedArray[0].length + x) * 4; // вычисление индекса для заполнения объекта ImageData
+
+                magnifierImageData.data[index] = brightness; // присваивание красного спектра
+                magnifierImageData.data[index + 1] = brightness; // приcваивание зеленого спектра
+                magnifierImageData.data[index + 2] = brightness; // присваивание синего спектра
+                magnifierImageData.data[index + 3] = 255; // присваивание прозрачности пикселя
+            }
+        }
+        context.putImageData(magnifierImageData, 0, 0); // отрисовка данных на холсте начиная с координат (0,0)
+        containerMagnifier.appendChild(magnifierCanvas);
     }
 
     // обработчик события движения курсора по картинке
@@ -95,6 +192,11 @@ const Canvas = ({file, shift}) => {
 
         setCoordinatesPicture(x, y);
         setBrightness(x, y);
+        const borderValue = Math.round(magnifierCanvas.height / (2 * zoom));
+        if ((x - borderValue > 0 && x + borderValue < 499) && (y - borderValue > 0 && y + borderValue < 2999)) {
+            setMagnifierPicture(x, y);
+        }
+
     }
 
     // обработчик события движения курсора в контейнере
@@ -110,7 +212,7 @@ const Canvas = ({file, shift}) => {
 
 
     useEffect(() => {
-        createImageFromMatrix(); // После получения файла и сдвига или изменения файла/сдвига, создаем изображение
+        handleUploadFile(); // После получения файла и сдвига или изменения файла/сдвига, создаем изображение
     }, [file, shift]);
 
 
@@ -150,13 +252,40 @@ const Canvas = ({file, shift}) => {
             </div>
 
             {
-                image &&
-                <div onMouseMove={handleMouseMoveContainer} className="pictureContainer">
-                    <div id="pictureContainer" onMouseMove={handleMouseMovePicture} className="pic">
-                        <img id="image" src={image.src} alt="Generated Image"/>
-                    </div>
-                </div>
+                overviewImage ?
+                    <div id="overviewContainer" className="overviewPicture">
+                        <img id="overviewImage" src={overviewImage.src} alt="overview"/>
+                    </div> : null
             }
+            {
+                image ?
+                    <div onMouseMove={handleMouseMoveContainer} className="pictureContainer">
+                        <div id="pictureContainer" onMouseMove={handleMouseMovePicture} className="pic">
+                            <img id="image" src={image.src} alt="Generated" style={{marginBottom: "-4px"}}/>
+                        </div>
+                    </div> :
+                    <div className="pictureContainer">
+                        <span>Загрузите изображения</span>
+                    </div>
+            }
+            <div className="zoom__container">
+                <p className="coordinates__item">Инструмент "Лупа"</p>
+                <form className="checkbox__container">
+                    <div>
+                        <input id="normalize-brightness" type="checkbox" checked={isNormalize} onChange={() => setIsNormalize(!isNormalize)} />
+                        <label htmlFor="normalize-brightness">Нормировать яркость</label>
+                    </div>
+                    <div>
+                        <input id="interpolate" type="checkbox" checked={isInterpolate} onChange={() => setIsInterpolate(!isInterpolate)}/>
+                        <label htmlFor="interpolate">Интерполировать</label>
+                    </div>
+                </form>
+
+                <div className="magnifier__area" id="magnifier-container"></div>
+                <label htmlFor="id">Увеличить в {zoom} раза</label>
+                <input id="zoom" type="range" min="4" max="20" value={zoom} onChange={(e) =>
+                    setZoom(parseInt(e.target.value))} step="8"/>
+            </div>
         </div>
 
     );
--- src/functions/functionaityCore.js
+++ src/functions/functionaityCore.js
@@ -1,36 +1,18 @@
-// наложение маски для получения истинного значения яркости пикселя в десятибитовом представлении
-const convertToTenBit = (item) => {
-    //получение информативных младших 10 бит из двухбайтового представления цвета
-    return item & 0b1111111111;
-}
-
-// получение ширины изображения
-const getImageWidth = (binaryArray) => {
-    const WIDTH_INDEX = 0; // индекс значения ширины изображения в бинарном массиве
-    return parseInt(binaryArray[WIDTH_INDEX]);
-}
-
-// получение высоты изображения
-const getImageHeight = (binaryArray) => {
-    const HEIGHT_INDEX = 1; // индекс значения высоты изображение в бинарном массиве
-    return parseInt(binaryArray[HEIGHT_INDEX]);
-}
-
 
 // получение изначального массива яркости пикселей (каждая яркость пикселя - 10 бит)
 export const getInitialBrightnessArray = (event) => {
     const fileData = event.target.result; //получение данных из бинарного файла
     let initialBrightnessArray = []; // инициализация массива яркости пикселей
     let binaryArray = new Uint16Array(fileData); //бинарный массив, который хранит по 16 бит информации, полученной из бинарного файла
-    let imageWidth = getImageWidth(binaryArray); // получение ширины изображения
+    let imageWidth = binaryArray[0]; // получение ширины изображения
 
-    let imageHeight = getImageHeight(binaryArray); // получение высоты изображения
+    let imageHeight = binaryArray[1]; // получение высоты изображения
     let dataIndex = 2; // индекс, с которого будет происходить считывание информации о яркости каждого пикселя из массива бинарных чисел
 
     for (let y = 0; y < imageHeight; y++) { // цикл составляющий массив яркости пикселей
         let row = []; //создание строки масивва яркости пикселя
         for (let x = 0; x < imageWidth; x++) {
-            row.push(convertToTenBit(binaryArray[dataIndex]));  // добавление в строку массива информативой части яркости пикселя
+            row.push(binaryArray[dataIndex] & 0b1111111111);  // добавление в строку массива информативой части яркости пикселя
             dataIndex++;
         }
         initialBrightnessArray.push(row); // добавление строки в массив яркости
@@ -42,20 +24,131 @@ export const getInitialBrightnessArray = (event) => {
 // аргументы функции: массив яркости пикселей (в представлении 10 бит, т.е. от 0 до 1023) и сдвиг
 export const getConvertedBrightnessArray = (initialBrightnessArray, shift) => {
     let pixelBrightnessMatrix = []; // инициализация массива, являющийся результатом работы функции
-    for (let i = 0; i < initialBrightnessArray.length; i++) {
+    for (let y = 0; y < initialBrightnessArray.length; y++) {
         let rowMatrix = []
-        for (let j = 0; j < initialBrightnessArray[0].length; j++) {
-            let red = (initialBrightnessArray[i][j] >> shift) & 0b11111111; // вычисление красного спектра
-            // вычисление зеленого спектра
-            let green = ((initialBrightnessArray[i][j] >> shift) & 0b11111111) | (initialBrightnessArray[i][j] >> (8 + shift));
-            let blue = green; // синий спектр == зеленый спектр
-            rowMatrix.push({Red: red, Green: green, Blue: blue});
+        for (let x = 0; x < initialBrightnessArray[0].length; x++) {
+            rowMatrix.push((initialBrightnessArray[y][x] >> shift) & 0xFF);
         }
         pixelBrightnessMatrix.push(rowMatrix);
     }
     return pixelBrightnessMatrix
 }
 
+// содание иходной матрицы обзорного изображения (яркость пикселя от 0 до 1023)
+export const getReducedInitBrightness = (initialBrightnessArray) => {
+    const reducedInitBrightness = [];
+    const DECREASE = 5;
+
+    for (let y = 0; y <= initialBrightnessArray.length - DECREASE; y+=DECREASE) {
+        let rowArray = [];
+        for (let x = 0; x <= initialBrightnessArray[0].length - DECREASE; x += DECREASE) {
+            const subArray = initialBrightnessArray.slice(y, y + DECREASE).map(row => row.slice(x, x + DECREASE));
+            let sum = subArray.flat().reduce((answer, item) => answer + item, 0)
+            rowArray.push(Math.round(sum/(DECREASE*DECREASE)));
+        }
+        reducedInitBrightness.push(rowArray);
+    }
+    return reducedInitBrightness;
+}
+
+
+export const getNearestNeighbor = (convertedColor, zoom, delta) => {
+    let zoomArray = [];
+    // console.log(delta)
+    for (let y = 0; y < convertedColor.length; y++) {
+        const row = [];
+        for (let x = 0 ; x < convertedColor[0].length; x++) {
+            for (let countRepeat = 0; countRepeat < zoom; countRepeat++) {
+                row.push(convertedColor[y][x]);
+            }
+        }
+        for (let countRepeat = 0; countRepeat < zoom; countRepeat++) {
+            zoomArray.push(row.slice());
+        }
+    }
+    if (delta === 0) {
+        return zoomArray;
+    } else return zoomArray.slice(delta, zoomArray.length - delta).map(row => row.slice(delta, row.length - delta));
+
+}
+
+export const getInterpolateArray  = (zoom, increaseArray) => {
+    const interpolateArray = [];
+    for (let y = 0; y < increaseArray.length - zoom; y++) {
+        const newRow = [];
+        for (let x = 0; x < increaseArray[0].length; x++) {
+
+            const I1 = increaseArray[y][x];
+            const I2 = increaseArray[y][x + zoom];
+            const I3 = increaseArray[y + zoom][x];
+            const I4 = increaseArray[y + zoom][x + zoom];
+            let Xloc = (x % zoom) / zoom;
+            let Yloc = (y % zoom) / zoom;
+
+            let d = I1;
+            let a = I2 - d;
+            let b = I3 - d;
+            let c = I4 - a - b - d;
+            let Iloc;
+
+            if (x >= increaseArray.length - zoom) {
+                Iloc = b * Yloc + d;
+            } else {
+                Iloc = a * Xloc + b * Yloc + c * Xloc * Yloc + d;
+            }
+            newRow.push(Iloc);
+        }
+        interpolateArray.push(newRow);
+    }
+    // обработка случая при зуме последних строк изображения
+    for (let y = increaseArray.length - zoom; y < increaseArray.length; y++) {
+        const newRow = [];
+        for (let x = 0; x < increaseArray[0].length; x++) {
+            const I1 = increaseArray[y][x];
+            const I2 = increaseArray[y][x + zoom];
+
+            const Xloc = (x % zoom) / zoom;
+
+            let d = I1;
+            let a = I2 - d;
+
+            let Iloc = a * Xloc + d;
+            newRow.push(Iloc);
+        }
+        interpolateArray.push(newRow);
+    }
+    return interpolateArray;
+}
+
+export const findMinMaxValue = (array) => {
+    try {
+        let minValue = array[0][0];
+        let maxValue = array[0][0];
+        for (let y = 0; y < array.length; y++) {
+            for (let x = 0; x < array[0].length; x++) {
+                minValue = Math.min(minValue, array[y][x]);
+                maxValue = Math.max(maxValue, array[y][x]);
+            }
+        }
+        return {minValue, maxValue}
+    } catch (e) {
+        console.log('error');
+    }
+
+}
+
+export const getNormalizeColorArray = (initialBrightnessArray, minValue, maxValue) => {
+    const normalizeColorArray = [];
+    for (let y = 0; y < initialBrightnessArray.length; y++) {
+        const rowNormalizeColorArray = [];
+        for (let x = 0; x < initialBrightnessArray[0].length; x++) {
+            const normalizePixel = Math.round((initialBrightnessArray[y][x] - minValue) / maxValue * 255);
+            rowNormalizeColorArray.push(normalizePixel);
+        }
+        normalizeColorArray.push(rowNormalizeColorArray);
+    }
+    return normalizeColorArray;
+}
 
 
 
